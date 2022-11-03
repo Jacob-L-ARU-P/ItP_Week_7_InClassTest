42e98276914e89e0e5e2aebdb7669d4434a8a94c
Jacob Lummis <jl1143@student.aru.ac.uk>
2022-11-03 14:16:35 +0000

    Task 3 start

--- Task 3/RectanglesNdTriangles.cs
+++ Task 3/RectanglesNdTriangles.cs
@@ -7,3 +7,43 @@
 // Code by Jacob Lummis
 // ARUP SID: 2213276
 // ---------------------
+// Declare Variable(s)
+
+int UserChoice = 0;
+
+// Program Start Fluff
+
+// Program Main Loop
+while (UserChoice != 9)
+{
+// Error Handling
+    try
+    {
+// User Inputs and Choices
+        Console.Write("\n\tPlease chose an Option: ");
+        switch(UserChoice = Convert.ToInt32(Console.ReadLine()))
+        {
+            case 1:
+            {
+                
+                break;
+            }
+            default:
+            {
+                Console.WriteLine("\tInvalid input detected, please try again.");
+                break;
+            }
+        }
+    } 
+// What to do if Broke    
+    catch (FormatException)
+    {
+        Console.WriteLine("\n\tException Thrown. Please only enter numbers using 0-9 (Zero to Nine) and '.' (full stops)");
+        UserChoice = 0;
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("\n\tException Thrown. Please only enter numbers using 0-9 (Zero to Nine) and '.' (full stops)");
+        UserChoice = 0;
+    }
+}
---
+++ Task 3/Shape.cs
@@ -0,0 +1,27 @@
+// ---------------------
+// Intro to Programming
+// Week 7 In Class Test
+// Task 3: Calculates area, perimeter and Square-ness 
+// ------- of Rectangles from 2 user inputs, can also
+// ------- do Triangles and determine their type.
+// Code by Jacob Lummis
+// ARUP SID: 2213276
+// ---------------------
+// Class file for task 3
+
+public class MyRectangle
+{
+    void RectangleArea()
+    {
+        
+    }
+    void RectanglePerimeter()
+    {
+
+    }
+}
+
+public class MyTriangle
+{
+
+}
