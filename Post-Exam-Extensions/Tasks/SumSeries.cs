7fcbc05cef49cd310ae811a4cec8ecdfe5bea9d5
Jacob Lummis <jl1143@student.aru.ac.uk>
2022-11-08 10:21:18 +0000

    Start of Post-Exam completionism

---
+++ Post-Exam-Extensions/Tasks/SumSeries.cs
@@ -0,0 +1,25 @@
+// ---------------------
+// Task 1: Series Series Calculator
+// ------- Calculates Series of X numbers, input by user
+// Code by Jacob Lummis
+// ARUP SID: 2213276
+// ---------------------
+// Part 1; Example Series
+void ESS2()
+{
+    // Static/Dependant Variables
+    float[] TestSeries2 = new float[5] {3.4f, 6.7f, 8.3f, 9.0f, 2.4f};
+    float TestSeriesSum2 = 0;
+    // Possible Pre-Loop Formatting
+
+    // The Example Loop
+    for(int i = 0; i < 5; i++)
+    {
+        TestSeriesSum2 =  TestSeriesSum2 + TestSeries2[i];
+        Console.WriteLine("\tValue " + (i + 1) + " of Example Series is: " + TestSeries2[i]);
+    }
+    TestSeriesSum2 = TestSeriesSum2 / TestSeries2.Length;
+    Console.WriteLine("\n\tSum of Example Series is: " + TestSeriesSum2);    
+}
+
+// Part 2; User Series Class
---
+++ Post-Exam-Extensions/TheProgram.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------
+// Intro to Programming
+// Week 8 Post-Exam extension program
+// Because I can get with the Methods, Man
+// "TheProgram" Object Oriented Everything
+// Able to call all 4 tasks, now new and improved!
+// -- Screen clearing to reduce amount of console-app scrolling required
+// ---------------------
+// Code by Jacob Lummis
+// ARUP SID: 2213276
+// ---------------------
